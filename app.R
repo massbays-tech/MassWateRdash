f21db7a192384fef2e83a2ab9b80e57e5d0f1623
fawda123 <mbafs2012@gmail.com>
2024-11-20 08:51:55 -0500

    add outlier plot, site plot, more widgets

--- app.R
+++ app.R
@@ -11,83 +11,96 @@ ui <- page_navbar(
   
   # upload & validate -----
   nav_panel("Upload & Validate",
-            page_sidebar(
-              sidebar = sidebar(
-                title = "Upload Data Files",
-                width = 400,
-                shinyWidgets::materialSwitch('tester', "Test mode", FALSE),
-                fileInput("resdat", "Upload Results Data (.xlsx)", accept = ".xlsx"),
-                fileInput("accdat", "Upload DQO Accuracy Data (.xlsx)", accept = ".xlsx"),
-                fileInput("frecomdat", "Upload DQO Frequency & Completeness Data (.xlsx)", accept = ".xlsx"),
-                fileInput("sitdat", "Upload Site Data (.xlsx)", accept = ".xlsx"),
-                fileInput("wqxdat", "Upload WQX Meta Data (.xlsx)", accept = ".xlsx")
-              ),
-              
-              layout_columns(
-                fill = FALSE,
-                value_box(
-                  title = "Results Data",
-                  value = htmlOutput("resdat_status")
-                ),
-                value_box(
-                  title = "Accuracy Data",
-                  value = htmlOutput("accdat_status")
-                ),
-                value_box(
-                  title = "Frequency & Completeness Data",
-                  value = htmlOutput("frecomdat_status")
-                ),
-                value_box(
-                  title = "Sites Data",
-                  value = htmlOutput("sitdat_status")
-                ),
-                value_box(
-                  title = "WQX Data",
-                  value = htmlOutput("wqxdat_status")
-                )
-              ),
-              
-              card(
-                card_header("Data Validation Messages"),
-                verbatimTextOutput("validation_messages", placeholder = FALSE)
-              )
-            )
-  ),
-  
-  # Visualize -----
-  nav_panel("Visualize",
             
-            page_sidebar(
-              sidebar = sidebar(
-                title = "Plot options",
-                width = 400,
-                uiOutput("prm1"),
-                selectInput("thresh", "Treshold type", choices = c('fresh', 'marine', 'none')),
-                selectInput("type", "Plot type", choices = c("box", "jitterbox", "bar", "jitterbar", "jitter"))
-              ),
-            
-              navset_card_underline(
-                full_screen = T,
-                nav_panel(
-                  "By season",
-                  plotOutput("season_plot")
-                ),
-                nav_panel(
-                  "By site",
-                  plotOutput("site_plot")
-                )
-              )
- 
-            )
+    page_sidebar(
+      
+      sidebar = sidebar(
+        title = "Upload Data Files",
+        width = 400,
+        shinyWidgets::materialSwitch('tester', "Test mode", FALSE),
+        fileInput("resdat", "Upload Results Data (.xlsx)", accept = ".xlsx"),
+        fileInput("accdat", "Upload DQO Accuracy Data (.xlsx)", accept = ".xlsx"),
+        fileInput("frecomdat", "Upload DQO Frequency & Completeness Data (.xlsx)", accept = ".xlsx"),
+        fileInput("sitdat", "Upload Site Data (.xlsx)", accept = ".xlsx"),
+        fileInput("wqxdat", "Upload WQX Meta Data (.xlsx)", accept = ".xlsx")
+      ),
+      
+      layout_columns(
+        fill = FALSE,
+        value_box(
+          title = "Results Data",
+          value = htmlOutput("resdat_status")
+        ),
+        value_box(
+          title = "Accuracy Data",
+          value = htmlOutput("accdat_status")
+        ),
+        value_box(
+          title = "Frequency & Completeness Data",
+          value = htmlOutput("frecomdat_status")
+        ),
+        value_box(
+          title = "Sites Data",
+          value = htmlOutput("sitdat_status")
+        ),
+        value_box(
+          title = "WQX Data",
+          value = htmlOutput("wqxdat_status")
+        )
+      ),
+      
+      card(
+        card_header("Data Validation Messages"),
+        verbatimTextOutput("validation_messages", placeholder = FALSE)
+      )
+      
+    )
             
   ),
   
   # QC reporting -----
-  nav_panel("QC reporting",
-            card(
-              card_header("Title"),
-              p("Description")
+  nav_panel("QC reporting and outlier assessment",
+      
+    navset_card_underline(
+      full_screen = F,
+      
+      nav_panel(
+        "QC report",
+        NULL
+      ),
+      
+      nav_panel(
+        "Outliers",
+        page_sidebar(
+          
+          sidebar = sidebar(
+            title = "Options",
+            width = 400,
+            uiOutput("prm1"),
+            uiOutput("dtrng1"),
+            selectInput("group1", "Group by", choices = c("month", "week", "site")),
+            selectInput("type1", "Plot type", choices = c("box", "jitterbox", "jitter"))
+          ),
+          navset_card_underline(
+            full_screen = T,
+            nav_panel(
+              "Plot",
+              plotOutput("outlier_plot")
+            ),
+            nav_panel(
+              "Table",
+              reactable::reactableOutput("outlier_table")
+            ),
+            nav_panel(
+              "Report",
+              NULL
             )
+          )
+        )
+      )
+      
+    )
+          
   ),
   
   # WQX output -----
@@ -96,6 +109,41 @@ ui <- page_navbar(
               card_header("Title"),
               p("Description")
             )
+  ),
+  
+  # Visualize -----
+  nav_panel("Visualize",
+            
+    page_sidebar(
+      sidebar = sidebar(
+        title = "Plot options",
+        width = 400,
+        uiOutput("prm2"),
+        uiOutput("dtrng2"),
+        selectInput("thresh", "Treshold type", choices = c('fresh', 'marine', 'none')),
+        selectInput("type2", "Plot type (season, site plots only)", choices = c("box", "jitterbox", "bar", "jitterbar", "jitter")),
+        selectInput("confint2", "Show confidence", choices = c(F, T)),
+        selectInput("group2", "Plot grouping (date plot only)", choices = c("site", "locgroup", "all"))
+      ),
+      
+      navset_card_underline(
+        full_screen = T,
+        nav_panel(
+          "Season",
+          plotOutput("season_plot")
+        ),
+        nav_panel(
+          "Date",
+          plotOutput("date_plot")
+        ),
+        nav_panel(
+          "Site",
+          plotOutput("site_plot")
+        )
+      )
+      
+    )
+            
   ),
   
   nav_spacer(),
@@ -107,11 +155,80 @@ ui <- page_navbar(
       "Source Code"
     )
   )
+  
 )
 
 # server -----
 server <- function(input, output, session) {
   
+  # reactive UI -----
+  output$prm1 <- renderUI({
+    
+    # inputs
+    fset <- fsetls()
+    
+    validate(
+      need(!is.na(fset$res), 'Waiting for input data...')
+    )
+    
+    tosel <- sort(unique(fset$res$`Characteristic Name`))
+    
+    selectInput("param1", "Parameter", choices = tosel)
+    
+  })
+  
+  output$prm2 <- renderUI({
+    
+    # inputs
+    fset <- fsetls()
+    
+    validate(
+      need(!is.na(fset$res), 'Waiting for input data...')
+    )
+    
+    tosel <- sort(unique(fset$res$`Characteristic Name`))
+    
+    selectInput("param2", "Parameter", choices = tosel)
+    
+  })
+  
+  output$dtrng1 <- renderUI({
+    
+    # inputs
+    param1 <- input$param1
+    fset <- fsetls()
+    
+    req(param1)
+    
+    tosel <- fset$res |> 
+      dplyr::filter(`Characteristic Name` == param1) |> 
+      dplyr::pull(`Activity Start Date`) |> 
+      range() |> 
+      as.Date()
+    
+    sliderInput("dtrng1", "Date range", min = tosel[1], max = tosel[2], value = tosel)
+    
+  })
+  
+  output$dtrng2 <- renderUI({
+    
+    # inputs
+    param2 <- input$param2
+    fset <- fsetls()
+    
+    req(param2)
+    
+    tosel <- fset$res |> 
+      dplyr::filter(`Characteristic Name` == param2) |> 
+      dplyr::pull(`Activity Start Date`) |> 
+      range() |> 
+      as.Date()
+    
+    sliderInput("dtrng2", "Date range", min = tosel[1], max = tosel[2], value = tosel)
+    
+  })
+  
+  # upload & validate -----
   # Reactive values to store validation messages and data states
   validation_log <<- reactiveVal("")
   data_states <<- reactiveValues(
@@ -205,31 +322,74 @@ server <- function(input, output, session) {
     
   })
   
-  output$prm1 <- renderUI({
+  # QC reporting -----
+  output$outlier_plot <- renderPlot({
     
     # inputs
-    fset <- fsetls()
+    param1 <- input$param1
+    dtrng1 <- as.character(input$dtrng1)
+    group1 <- input$group1
+    type1 <- input$type1
     
-    validate(
-      need(!is.na(fset$res), 'Waiting for input data...')
-    )
-  
-    tosel <- sort(unique(fset$res$`Characteristic Name`))
-
-    selectInput("param1", "Parameter", choices = tosel)
+    req(dtrng1)
+    
+    anlzMWRoutlier(fset = fsetls(), param = param1, group = group1, type = type1, dtrng = dtrng1, bssize = 18)
     
   })
   
+  output$outlier_table <- reactable::renderReactable({
+    
+    # inputs
+    param1 <- input$param1
+    dtrng1 <- as.character(input$dtrng1)
+    group1 <- input$group1
+    type1 <- input$type1
+    
+    req(dtrng1)
+    
+    tab <- anlzMWRoutlier(fset = fsetls(), param = param1, group = group1, dtrng = dtrng1, outliers = T)
+    
+    out <- reactable::reactable(
+      tab,
+      defaultColDef = reactable::colDef(
+        footerStyle = list(fontWeight = "bold"),
+        resizable = TRUE
+      ),
+      filterable = T
+    )
+    
+    return(out)
+                              
+  })
+  
+  # Visualize ----
   output$season_plot <- renderPlot({
     
     # inputs
     thresh <- input$thresh
-    param1 <- input$param1
-    type <- input$type
+    param2 <- input$param2
+    dtrng2 <- as.character(input$dtrng2)
+    type2 <- input$type2
+    confint2 <- as.logical(input$confint2)
     
-    req(param1)
+    req(dtrng2)
+
+    anlzMWRseason(fset = fsetls(), param = param2, thresh = thresh, type = type2, dtrng = dtrng2, confint = confint2, bssize = 18)
     
-    anlzMWRseason(fset = fsetls(), param = param1, thresh = thresh, type = type, bssize = 18)
+  })
+  
+  output$date_plot <- renderPlot({
+    
+    # inputs
+    thresh <- input$thresh
+    param2 <- input$param2
+    dtrng2 <- as.character(input$dtrng2)
+    group2 <- input$group2
+    confint2 <- as.logical(input$confint2)
+    
+    req(dtrng2)
+    
+    anlzMWRdate(fset = fsetls(), param = param2, thresh = thresh, group = group2, dtrng = dtrng2, confint = confint2, bssize = 18)
     
   })
   
@@ -237,12 +397,14 @@ server <- function(input, output, session) {
     
     # inputs
     thresh <- input$thresh
-    param1 <- input$param1
-    type <- input$type
+    param2 <- input$param2
+    dtrng2 <- as.character(input$dtrng2)
+    type2 <- input$type2
+    confint2 <- as.logical(input$confint2)
     
-    req(param1)
+    req(dtrng2)
     
-    anlzMWRsite(fset = fsetls(), param = param1, thresh = thresh, type = type, bssize = 18)
+    anlzMWRsite(fset = fsetls(), param = param2, thresh = thresh, type = type2, dtrng = dtrng2, confint = confint2, bssize = 18)
     
   })
   
